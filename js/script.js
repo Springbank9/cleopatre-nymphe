cce5f8fa4b21dab7b12647c0601141738bf7b4fc
Springbank9 <isabellahay7@gmail.com>
2021-06-18 10:05:47 +0000

    added sticky header and content to the page and uploaded additional images

--- js/script.js
+++ js/script.js
@@ -1,40 +1,75 @@
-/*global $ */
-$(document).ready(function() {
+class StickyNavigation {
+	
+	constructor() {
+		this.currentId = null;
+		this.currentTab = null;
+		this.tabContainerHeight = 70;
+		let self = this;
+		$('.et-hero-tab').click(function() { 
+			self.onTabClick(event, $(this)); 
+		});
+		$(window).scroll(() => { this.onScroll(); });
+		$(window).resize(() => { this.onResize(); });
+	}
+	
+	onTabClick(event, element) {
+		event.preventDefault();
+		let scrollTop = $(element.attr('href')).offset().top - this.tabContainerHeight + 1;
+		$('html, body').animate({ scrollTop: scrollTop }, 600);
+	}
+	
+	onScroll() {
+		this.checkTabContainerPosition();
+    this.findCurrentTabSelector();
+	}
+	
+	onResize() {
+		if(this.currentId) {
+			this.setSliderCss();
+		}
+	}
+	
+	checkTabContainerPosition() {
+		let offset = $('.et-hero-tabs').offset().top + $('.et-hero-tabs').height() - this.tabContainerHeight;
+		if($(window).scrollTop() > offset) {
+			$('.et-hero-tabs-container').addClass('et-hero-tabs-container--top');
+		} 
+		else {
+			$('.et-hero-tabs-container').removeClass('et-hero-tabs-container--top');
+		}
+	}
+	
+	findCurrentTabSelector(element) {
+		let newCurrentId;
+		let newCurrentTab;
+		let self = this;
+		$('.et-hero-tab').each(function() {
+			let id = $(this).attr('href');
+			let offsetTop = $(id).offset().top - self.tabContainerHeight;
+			let offsetBottom = $(id).offset().top + $(id).height() - self.tabContainerHeight;
+			if($(window).scrollTop() > offsetTop && $(window).scrollTop() < offsetBottom) {
+				newCurrentId = id;
+				newCurrentTab = $(this);
+			}
+		});
+		if(this.currentId != newCurrentId || this.currentId === null) {
+			this.currentId = newCurrentId;
+			this.currentTab = newCurrentTab;
+			this.setSliderCss();
+		}
+	}
+	
+	setSliderCss() {
+		let width = 0;
+		let left = 0;
+		if(this.currentTab) {
+			width = this.currentTab.css('width');
+			left = this.currentTab.offset().left;
+		}
+		$('.et-hero-tab-slider').css('width', width);
+		$('.et-hero-tab-slider').css('left', left);
+	}
+	
+}
 
-  "use strict";
-
-  $('.menu > ul > li:has( > ul)').addClass('menu-dropdown-icon');
-  //Checks if li has sub (ul) and adds class for toggle icon - just an UI
-
-  $('.menu > ul > li > ul:not(:has(ul))').addClass('normal-sub');
-  //Checks if drodown menu's li elements have anothere level (ul), if not the dropdown is shown as regular dropdown, not a mega menu (thanks Luka Kladaric)
-
-  $(".menu > ul").before("<a href=\"#\" class=\"menu-mobile\">Navigation</a>");
-
-  //Adds menu-mobile class (for mobile toggle menu) before the normal menu
-  //Mobile menu is hidden if width is more then 959px, but normal menu is displayed
-  //Normal menu is hidden if width is below 959px, and jquery adds mobile menu
-  //Done this way so it can be used with wordpress without any trouble
-
-  $(".menu > ul > li").hover(function(e) {
-    if ($(window).width() > 943) {
-      $(this).children("ul").stop(true, false).fadeToggle(150);
-      e.preventDefault();
-    }
-  });
-  //If width is more than 943px dropdowns are displayed on hover
-
-  $(".menu > ul > li").click(function() {
-    if ($(window).width() <= 943) {
-      $(this).children("ul").fadeToggle(150);
-    }
-  });
-  //If width is less or equal to 943px dropdowns are displayed on click (thanks Aman Jain from stackoverflow)
-
-  $(".menu-mobile").click(function(e) {
-    $(".menu > ul").toggleClass('show-on-mobile');
-    e.preventDefault();
-  });
-  //when clicked on mobile-menu, normal menu is shown as a list, classic rwd menu story (thanks mwl from stackoverflow)
-
-});
+new StickyNavigation();
